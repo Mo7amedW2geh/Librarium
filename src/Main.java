87efdde63b458ced0ac2e5e8c7a1e528b92b33a9
Marco Tharwat <markotharwat11@gmail.com>
2024-11-08 22:22:21 +0200

    wassup!

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
     }
 
     static {
-        Book book1 = new Book("Book1", "Mystery", "des", 7, "/books/Book1.png");
-        Book book2 = new Book("Book2", "Mystery", "des", 7, "/books/Book2.png");
-        Book book3 = new Book("Book3", "Fantasy", "des", 7, "/books/Book3.png");
-        Book book4 = new Book("Book4", "Fantasy", "des", 7, "/books/Book4.png");
-        Book book5 = new Book("Book5", "Fantasy", "des", 7);
-        Book book6 = new Book("Book6", "Fantasy", "des", 7);
-        Book book7 = new Book("Book7", "Fantasy", "des", 7);
-        Book book8 = new Book("Book8", "Fantasy", "des", 7);
-        Book book9 = new Book("Book9", "Fantasy", "des", 7);
+        new Book("Book1", "Mystery", "des", 7, "/books/Book1.png");
+        new Book("Book2", "Mystery", "des", 7, "/books/Book2.png");
+        new Book("Book3", "Fantasy", "des", 7, "/books/Book3.png");
+        new Book("Book4", "Fantasy", "des", 7, "/books/Book4.png");
+        new Book("Book5", "Fantasy", "des", 7);
+        new Book("Book6", "Fantasy", "des", 7);
+        new Book("Book7", "Fantasy", "des", 7);
+        new Book("Book8", "Fantasy", "des", 7);
+        new Book("Book9", "Fantasy", "des", 7);
     }
 }
